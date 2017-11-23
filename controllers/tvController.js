70e7080be857cd9a60420bc4505d3e95325fd8b2
tavo <coxlaj@bdgsa.net>
2017-11-22 19:52:11 -0600

    Promesaception

--- controllers/tvController.js
+++ controllers/tvController.js
@@ -2,11 +2,37 @@ const mongoose = require('mongoose');
 const TVShow = mongoose.model('TVShow');
 
 const obj = {};
+const arrtvshows = [
+    {
+        pais: "GT",
+        titulo: "abc",
+        anio: 2001
+    },
+    {
+        pais: "GT",
+        titulo: "jfasfj",
+        anio: 2002
+    }
+];
+
+const todos = (tvshows) => {
+    return new Promise((resolve, reject) => {
+        if(tvshows.length < 0){
+            return reject('No hay datos');
+        }
+        return resolve({data: tvshows.length});
+    });
+    //return TVShow.find();
+};
 
 obj.getArray = (req, res, next) => {
     TVShow.find()
-    .then(tvshows => res.send(tvshows))
+    .then(tvshows => todos(tvshows))
+    .then(resultado => res.send(resultado))
     .catch(err => res.send({error: err}));
+    /*todos()
+    .then(tvshows => res.send(tvshows))
+    .catch(err => res.send({error: err}));*/
 };
 
 obj.postArray = (req, res, next)=>{
